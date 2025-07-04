100543544447da591d8c9f3f0dfc43f7ce0a4e62
Stefania Rincu <Stefania.Rincu@softwire.com>
2025-07-04 13:35:44 +0300

    refactor code

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -3,7 +3,7 @@ export class Item {
     sellIn: number;
     quality: number;
 
-    constructor(name, sellIn, quality) {
+    constructor(name: string, sellIn: number, quality: number) {
         this.name = name;
         this.sellIn = sellIn;
         this.quality = quality;
@@ -13,57 +13,44 @@ export class Item {
 export class GildedRose {
     items: Array<Item>;
 
-    constructor(items = [] as Array<Item>) {
+    constructor(items: Item[] = [] as Array<Item>) {
         this.items = items;
     }
 
-    updateQuality() {
-        for (let i = 0; i < this.items.length; i++) {
-            if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if (this.items[i].quality > 0) {
-                    if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                        this.items[i].quality = this.items[i].quality - 1
-                    }
-                }
-            } else {
-                if (this.items[i].quality < 50) {
-                    this.items[i].quality = this.items[i].quality + 1
-                    if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].sellIn < 11) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
-                            }
-                        }
-                        if (this.items[i].sellIn < 6) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
-                            }
-                        }
-                    }
-                }
-            }
-            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].sellIn = this.items[i].sellIn - 1;
-            }
-            if (this.items[i].sellIn < 0) {
-                if (this.items[i].name != 'Aged Brie') {
-                    if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].quality > 0) {
-                            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                                this.items[i].quality = this.items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        this.items[i].quality = this.items[i].quality - this.items[i].quality
-                    }
-                } else {
-                    if (this.items[i].quality < 50) {
-                        this.items[i].quality = this.items[i].quality + 1
-                    }
-                }
-            }
-        }
+    updateQuality(): Item[] {
+        for (let i: number = 0; i < this.items.length; i++) {
+            // If the item is 'Sulfuras', it should not update anything
+            if (this.items[i].name === 'Sulfuras, Hand of Ragnaros')
+                continue;
 
+            // Decrease sellIn by 1
+            this.items[i].sellIn -= 1;
+
+            // If the item is 'Backstage passes' and the sellIn has expired, set quality to 0 and skip
+            if (this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert' && this.items[i].sellIn <= 0) {
+                this.items[i].quality = 0;
+                continue;
+            }
+
+            // Define sign to illustrate increasing or decreasing depending on product type
+            const changeBySign: number = (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') ? -1 : 1
+            // Define weights to increase/decrease quality for items that are not a Backstage pass
+            const weightNotBackstagePass: number = (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert' && this.items[i].sellIn <= 0) ? 2 : 1
+            // Define weights to increase/decrease quality for items that are a Backstage pass
+            const weightBackstagePass: number = (this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert' && this.items[i].sellIn <= 5) ? 3 : (this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert' && this.items[i].sellIn <= 10) ? 2 : 1
+            // Define the final weight
+            const weight: number = (this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert') ? weightBackstagePass : weightNotBackstagePass;
+
+            // If quality is lower or equal then 0 and it should be decreased, don't do anything
+            if (this.items[i].quality <= 0 && changeBySign === -1)
+                continue;
+            // If quality is greater or equal then 50 and it should be increased, don't do anything
+            if (this.items[i].quality >= 50 && changeBySign === 1)
+                continue;
+
+            // Update quality
+            this.items[i].quality += changeBySign * weight;
+        }
         return this.items;
     }
 }
